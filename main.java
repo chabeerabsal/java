1895df2a6cb726161aa72456a389cef7ffb5e244
chabeer absal <chabeerabsal@gmail.com>
2021-07-27 15:47:01 +0530

    first

---
+++ main.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+
+    
+   public static void main(String []args){
+         
+       int number=10;
+         
+        
+       System.out.println(number);
+     
+}
+
+}
