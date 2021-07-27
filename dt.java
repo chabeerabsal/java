35fe510c94c8738bc65194f7e73bbd6519d58890
chabeer absal <chabeerabsal@gmail.com>
2021-07-27 15:49:29 +0530

    first

---
+++ dt.java
@@ -0,0 +1,30 @@
+public class datatypes{
+
+     
+	public static void main(String []args){
+         
+		byte number=15;
+         
+		short no1=150;
+         
+		long no2=12345656778990L;
+
+         	int no3=123345;
+	
+         float no4=112.77f;
+
+        	 double no5=1223344.46;
+
+         	char name='c';
+
+         	boolean bool=true;
+
+       	 	System.out.println(name);
+
+        	System.out.println(bool);
+
+        	System.out.println(no5);
+
+        	System.out.println(no2);
+     }
+}
